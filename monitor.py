2ace1d97835d7fb5ef5168fa50e038bf3cf3fbd1
leiweibau <105860611+leiweibau@users.noreply.github.com>
2023-01-31 20:35:05 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -12,29 +12,6 @@ import sqlite3
 con = sqlite3.connect("monitoring.db")
 cur = con.cursor()
 
-
-sql_create_table = """ CREATE TABLE Services_Events(
-                            moneve_URL TEXT NOT NULL,
-                            moneve_DateTime TEXT NOT NULL,
-                            moneve_StatusCode NUMERIC NOT NULL,
-                            moneve_Latency TEXT NOT NULL,
-                            PRIMARY KEY(moneve_URL)
-                        ); """
-#cur.execute(sql_create_table)
-
-sql_create_table = """  CREATE TABLE Services(
-                            mon_URL TEXT NOT NULL,
-                            mon_MAC TEXT,
-                            mon_LastStatus NUMERIC NOT NULL,
-                            mon_LastLatency TEXT NOT NULL,
-                            mon_LastScan TEXT NOT NULL,
-                            mon_Tags TEXT,
-                            mon_AlertEvents INTEGER DEFAULT 0,
-                            mon_AlertDown INTEGER DEFAULT 0,
-                            PRIMARY KEY(mon_URL)
-                        ); """
-#cur.execute(sql_create_table)
-
 #DELAY = 60  # Delay between site queries
 EMAIL_INTERVAL = 1800  # Delay between alert emails
 
@@ -56,31 +33,52 @@ Subject: Monitor Service Notification
 
 You are being notified that {site} is experiencing a {status} status!
 """
+# -----------------------------------------------------------------------------
+def prepare_service_monitoring_env ():
+    global con
+    global cur
+    print()
+    sql_create_table = """ CREATE TABLE IF NOT EXISTS Services_Events(
+                                moneve_URL TEXT NOT NULL,
+                                moneve_DateTime TEXT NOT NULL,
+                                moneve_StatusCode NUMERIC NOT NULL,
+                                moneve_Latency TEXT NOT NULL,
+                                PRIMARY KEY(moneve_URL)
+                            ); """
+    cur.execute(sql_create_table)
 
+    sql_create_table = """  CREATE TABLE IF NOT EXISTS Services(
+                                mon_URL TEXT NOT NULL,
+                                mon_MAC TEXT,
+                                mon_LastStatus NUMERIC NOT NULL,
+                                mon_LastLatency TEXT NOT NULL,
+                                mon_LastScan TEXT NOT NULL,
+                                mon_Tags TEXT,
+                                mon_AlertEvents INTEGER DEFAULT 0,
+                                mon_AlertDown INTEGER DEFAULT 0,
+                                PRIMARY KEY(mon_URL)
+                            ); """
+    cur.execute(sql_create_table)
+
+# -----------------------------------------------------------------------------
 def colorize(text, color):
     """Return input text wrapped in ANSI color codes for input color."""
     return COLOR_DICT[color] + str(text) + COLOR_DICT['end']
 
+# -----------------------------------------------------------------------------
+def service_monitoring_log(site, status, latency):
+    global log
 
-def error_log(site, status, latency):
-    """Log errors to stdout and log file, and send alert email via SMTP."""
-    # Print colored status message to terminal
-    print("({}) {} STATUS: {}... {}".format(strftime("%Y-%m-%d %H:%M:%S"),
-                                        site,
-                                        colorize(status, "yellow"),
-                                        latency
-                                        )
-         )
     # Log status message to log file
-    with open('monitor.log', 'a') as log:
-        log.write("({}) {} STATUS: {}... {}\n".format(strftime("%Y-%m-%d %H:%M:%S"),
+    #with open('monitor.log', 'a') as log:
+    monitor_logfile.write("{} | {} | {} | {}\n".format(strftime("%Y-%m-%d %H:%M:%S"),
                                                 site,
                                                 status,
                                                 latency,
                                                 )
                   )
 
-
+# -----------------------------------------------------------------------------
 def send_alert(site, status):
     """If more than EMAIL_INTERVAL seconds since last email, resend email"""
     if (time() - last_email_time[site]) > EMAIL_INTERVAL:
@@ -101,7 +99,7 @@ def send_alert(site, status):
         except smtplib.SMTPException:
             print(colorize("Error sending email ({}:{})".format(host, port), "red"))
 
-
+# -----------------------------------------------------------------------------
 def ping(site):
     # Enable self signed SSL
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -118,7 +116,7 @@ def ping(site):
         latency = "99999"
         return 503, latency
 
-
+# -----------------------------------------------------------------------------
 def get_sites():
     global cur
     global con
@@ -132,58 +130,71 @@ def get_sites():
 
     return sites
 
-def main():
+# -----------------------------------------------------------------------------
+def service_monitoring():
     global cur
     global con
-    # Empty Log
-    #open('monitor.log', 'w').close()
+    
+    # Empty Log and write new header
+    with open('monitor.log', 'w') as monitor_logfile:
+        monitor_logfile.write("Monitoring:\n---------------------------------------------------------\n\n")
+        monitor_logfile.write("Timestamp: " + strftime("%Y-%m-%d %H:%M:%S") + "\n\n")
+        monitor_logfile.write("| Timestamp | URL | StatusCode | ResponseTime |\n-----------------------------------------------")
+
     sites = get_sites()
 
     for site in sites:
-        #print(colorize("Beginning monitoring of {}".format(site), "bold"))
         last_email_time[site] = 0  # Initialize timestamp as 0
 
     while sites:
-        try:
-            for site in sites:
-                status,latency = ping(site)
-                if status == 200:
-                    print("({}) {} STATUS: {} ... {}".format(strftime("%Y-%m-%d %H:%M:%S"),
-                                        site,
-                                        colorize(status, "green"),latency
-                                        )
-                         )
-                    
-                    #sqlite_insert_with_param = """INSERT INTO Services
-                    #                  (mon_URL, mon_MAC, mon_LastStatus, mon_LastLatency, mon_LastScan, mon_Tags, mon_AlertEvents, mon_AlertDown) 
-                    #                  VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
+        for site in sites:
+            status,latency = ping(site)
+            if status == 200:
+                print("({}) {} STATUS: {} ... {}".format(strftime("%Y-%m-%d %H:%M:%S"),
+                                    site,
+                                    colorize(status, "green"),
+                                    latency
+                                    )
+                     )
+                service_monitoring_log(site, status, latency)
+                # sqlite_insert_with_param = """INSERT INTO Services
+                #                  (mon_URL, mon_MAC, mon_LastStatus, mon_LastLatency, mon_LastScan, mon_Tags, mon_AlertEvents, mon_AlertDown) 
+                #                  VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
 
-                    #data_tuple = (site, "s", status, latency, strftime("%Y-%m-%d %H:%M:%S"), "s", 1, 1)
-                    #cur.execute(sqlite_insert_with_param, data_tuple)
-                    #con.commit()
+                # data_tuple = (site, "s", status, latency, strftime("%Y-%m-%d %H:%M:%S"), "s", 1, 1)
+                # cur.execute(sqlite_insert_with_param, data_tuple)
+                # con.commit()
+                sys.stdout.flush()
+            else:
+                print("({}) {} STATUS: {}... {}".format(strftime("%Y-%m-%d %H:%M:%S"),
+                                    site,
+                                    colorize(status, "yellow"),
+                                    latency
+                                    )
+                     )
+                service_monitoring_log(site, status, latency)
+                # sqlite_insert_with_param = """INSERT INTO Services
+                #                   (mon_URL, mon_MAC, mon_LastStatus, mon_LastLatency, mon_LastScan, mon_Tags, mon_AlertEvents, mon_AlertDown) 
+                #                   VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
 
-                    sys.stdout.flush()
-                else:
-                    # sqlite_insert_with_param = """INSERT INTO Services
-                    #                   (mon_URL, mon_MAC, mon_LastStatus, mon_LastLatency, mon_LastScan, mon_Tags, mon_AlertEvents, mon_AlertDown) 
-                    #                   VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
+                # data_tuple = (site, "s", status, latency, strftime("%Y-%m-%d %H:%M:%S"), "s", 1, 1)
+                # cur.execute(sqlite_insert_with_param, data_tuple)
+                # con.commit()
 
-                    # data_tuple = (site, "s", status, latency, strftime("%Y-%m-%d %H:%M:%S"), "s", 1, 1)
-                    # cur.execute(sqlite_insert_with_param, data_tuple)
-                    # con.commit()
+                #send_alert(site, status)
 
-                    error_log(site, status, latency)
-                    #send_alert(site, status)
-            #sleep(DELAY)
-            exit()
-        # except requests.exceptions.ConnectionError:
-        #     print(colorize("No Connection ({})".format(site), "red"))
-        except KeyboardInterrupt:
-            print(colorize("\n-- Monitoring canceled --", "red"))
-            break
-    else:
-        print(colorize("No site(s) input to monitor!", "red"))
+        monitor_logfile.close()
         exit()
 
-#if __name__ == '__main__':
-main()
+    else:
+        with open('monitor.log', 'a') as monitor_logfile:
+            monitor_logfile.write("\n\nNo site(s) to monitor!")
+            monitor_logfile.close()
+        exit()
+
+# Workflow
+prepare_service_monitoring_env()
+service_monitoring()
+
+
+
